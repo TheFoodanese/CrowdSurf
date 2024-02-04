4cf713edc1f36e0b087ce0198d6015e8834ebc7d
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-02-04 16:41:56 +0000

    added functionality to create cards and clear button

--- asset/script.js
+++ asset/script.js
@@ -58,7 +58,7 @@ $(".container #cardsection").append(divrow);
 
 
 }
-searchByPerformer("Taylor Swift");
+// searchByPerformer("Taylor%20Swift");
 
 // <!-- Search for events by category eg concert or sport  -->
 function searchByCategory(type){
@@ -70,6 +70,41 @@ function searchByCategory(type){
       return response.json();
   }).then(function(data){
       console.log(data);
+
+
+for(i=0;i<data.events.length; i++){
+   //Getting data extracted for events
+    var eventSelector=data.events[i];
+    var eventType=eventSelector.type;
+    var eventDate=eventSelector.datetime_utc;
+    var eventAddress=eventSelector.venue.address;
+    var eventCity=eventSelector.venue.display_location;
+    var performers=eventSelector.performers[0];
+    var performerName=performers.name;
+    var performersImage=performers.image;
+    var performerUrl=performers.url
+    //Create Card
+    var divrow=$("<div>").attr("class","row");
+    var divcolm=$("<div>").attr("class","col s12 m7");
+    var divCard=$("<div>").attr("class","card");
+    var divImg=$("<div>").attr("class","card-image");
+    var divContent=$("<div>").attr("class","card-content").append($("<p>"));
+    var divAction=$("<div>").attr("class","card-action").append($("<a>"));
+    var imgTag=$("<img>");
+    imgTag.attr("src",performersImage);
+    divImg.append(imgTag);
+    divAction.find("a").attr("href",performerUrl).text("Click this link for more details");
+    divContent.find("p").text(`${performerName},${eventType},${eventDate},${eventAddress},${eventCity}` );
+
+    divCard.append(divImg,divContent,divAction);
+    divcolm.append(divCard);
+    divrow.append(divcolm);
+    $(".container #cardsection").append(divrow);
+
+
+
+
+}
   })
 
 
@@ -126,5 +161,12 @@ searchByCityChoice("London");
 
 
 
+var clearbtn=document.querySelector("#clearbtn");
+clearbtn.addEventListener("click",clear);
 
+function clear(event){
+    event.preventDefault();
+    $("#cardsection").empty();
+
+}
 
